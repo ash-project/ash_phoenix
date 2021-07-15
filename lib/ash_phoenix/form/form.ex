40380295ebefd4a6e1f4e81dcf54a31b312504c1
Zach Daniel <zach@zachdaniel.dev>
2021-07-15 13:31:19 -0400

    documentation

--- lib/ash_phoenix/form/form.ex
+++ lib/ash_phoenix/form/form.ex
@@ -1,12 +1,45 @@
 defmodule AshPhoenix.Form do
   @moduledoc """
-  An experimental new implementation for forms
+  A module to allow you to fluidly use resources with phoenix forms.
+
+  The general workflow is, with either liveview or phoenix forms:
+
+  1. Create a form with `AshPhoenix.Form`
+  2. Render that form with Phoenix's `form_for` (or, if using surface, <Form>)
+  3. To validate the form (e.g with `on-change` for liveview), pass the input to `AshPhoenix.Form.validate(form, params)`
+  4. On form submission, pass the input to `AshPhoenix.Form.validate(form, params)` and then use `AshPhoenix.Form.submid(form, ApiModule)`
+
+  If your resource action accepts related data, (for example a managed relationship argument, or an embedded resource attribute), you can
+  use Phoenix's `inputs_for` for that field, *but* you must explicitly configure the behavior of it using the `forms` option.
+  See `Form.for_create/3` for more.
+
+  For example:
+
+  ```elixir
+  form =
+    user
+    |> AshPhoenix.Form.for_update(:update, forms: [
+      profile: [
+        resource: MyApp.Profile,
+        data: user.profile,
+        create_action: :create,
+        update_action: :update
+        forms: [
+          emails: [
+            data: user.profile.emails,
+            resource: MyApp.UserEmail,
+            create_action: :create,
+            update_action: :update
+          ]
+        ]
+      ]
+    ])
+  ```
   """
   defstruct [
     :resource,
     :action,
     :type,
-    :fields,
     :params,
     :source,
     :name,
@@ -18,42 +51,88 @@ defmodule AshPhoenix.Form do
     :opts,
     :id,
     :transform_errors,
+    valid?: false,
     errors: false
   ]
 
-  import AshPhoenix.FormData.Helpers
+  @type t :: %__MODULE__{
+          resource: Ash.Resource.t(),
+          action: atom,
+          type: :create | :update | :destroy,
+          params: map,
+          source: Ash.Changeset.t(),
+          data: nil | Ash.Resource.record(),
+          form_keys: Keyword.t(),
+          forms: map,
+          method: String.t(),
+          submit_errors: Keyword.t() | nil,
+          opts: Keyword.t(),
+          transform_errors:
+            nil
+            | (Ash.Changeset.t(), error :: Ash.Error.t() ->
+                 [{field :: atom, message :: String.t(), substituations :: Keyword.t()}]),
+          valid?: boolean,
+          errors: boolean
+        }
 
-  def validate(form, new_params, opts \\ []) do
-    new_form_opts =
-      if Keyword.has_key?(opts, :errors) do
-        form.opts
-        |> Keyword.put(:errors, opts[:errors])
-        |> Keyword.put(:params, new_params)
-        |> Keyword.put(:forms, form.form_keys)
-      else
-        form.opts
-        |> Keyword.put(:errors, true)
-        |> Keyword.put(:params, new_params)
-        |> Keyword.put(:forms, form.form_keys)
-      end
+  @for_opts [
+    forms: [
+      type: :keyword_list,
+      doc: "Nested form configurations. See for_create/3 docs for more."
+    ],
+    as: [
+      type: :string,
+      default: "form",
+      doc:
+        "The name of the form in the submitted params. You will need to pull the form params out using this key."
+    ],
+    id: [
+      type: :string,
+      default: "form",
+      doc: "The html id of the form."
+    ],
+    transform_errors: [
+      type: {:fun, 2},
+      doc: """
+      Allows for manual manipulation and transformation of errors.
 
-    case form.type do
-      :create ->
-        for_create(
-          form.resource,
-          form.action,
-          new_form_opts
-        )
+      If possible, try to implement `AshPhoenix.FormData.Error` for the error (if it as a custom one, for example).
+      If that isn't possible, you can provide this function which will get the changeset and the error, and should
+      return a list of ash phoenix formatted errors, e.g `[{field :: atom, message :: String.t(), substituations :: Keyword.t()}]`
+      """
+    ],
+    method: [
+      type: :string,
+      doc:
+        "The http method to associate with the form. Defaults to `post` for creates, and `put` for everything else."
+    ]
+  ]
 
-      :update ->
-        for_update(form.data, form.action, new_form_opts)
+  @doc false
+  defp validate_opts_with_extra_keys(opts, schema) do
+    keys = Keyword.keys(schema)
 
-      :destroy ->
-        for_destroy(form.data, form.action, new_form_opts)
-    end
+    {opts, extra} = Keyword.split(opts, keys)
+
+    opts = Ash.OptionsHelpers.validate!(opts, schema)
+
+    Keyword.merge(opts, extra)
   end
 
-  def for_create(resource, action, opts \\ []) do
+  import AshPhoenix.FormData.Helpers
+
+  @doc """
+  Creates a form corresponding to a create action on a resource.
+
+  Options:
+  #{Ash.OptionsHelpers.docs(@for_opts)}
+
+  Any *additional* options will be passed to the underlying call to `Ash.Changeset.for_create/4`. This means
+  you can set things like the tenant/actor. These will be retained, and provided again when `Form.submit/3` is called.
+  """
+  @spec for_create(Ash.Resource.t(), action :: atom, opts :: Keyword.t()) :: t()
+  def for_create(resource, action, opts \\ []) when is_atom(resource) do
+    opts = validate_opts_with_extra_keys(opts, @for_opts)
     {forms, params} = handle_forms(opts[:params] || %{}, opts[:forms] || [], !!opts[:errors], nil)
 
     changeset_opts =
@@ -88,8 +167,19 @@ defmodule AshPhoenix.Form do
           changeset_opts
         )
     }
+    |> set_validity()
   end
 
+  @doc """
+  Creates a form corresponding to an update action on a record.
+
+  Options:
+  #{Ash.OptionsHelpers.docs(@for_opts)}
+
+  Any *additional* options will be passed to the underlying call to `Ash.Changeset.for_update/4`. This means
+  you can set things like the tenant/actor. These will be retained, and provided again when `Form.submit/3` is called.
+  """
+  @spec for_update(Ash.Resource.record(), action :: atom, opts :: Keyword.t()) :: t()
   def for_update(%resource{} = data, action, opts \\ []) do
     {forms, params} =
       handle_forms(opts[:params] || %{}, opts[:forms] || [], !!opts[:errors], data)
@@ -119,8 +209,19 @@ defmodule AshPhoenix.Form do
           changeset_opts
         )
     }
+    |> set_validity()
   end
 
+  @doc """
+  Creates a form corresponding to a destroy action on a record.
+
+  Options:
+  #{Ash.OptionsHelpers.docs(@for_opts)}
+
+  Any *additional* options will be passed to the underlying call to `Ash.Changeset.for_destroy/4`. This means
+  you can set things like the tenant/actor. These will be retained, and provided again when `Form.submit/3` is called.
+  """
+  @spec for_destroy(Ash.Resource.record(), action :: atom, opts :: Keyword.t()) :: t()
   def for_destroy(%resource{} = data, action, opts \\ []) do
     {forms, params} =
       handle_forms(opts[:params] || %{}, opts[:forms] || [], !!opts[:errors], data)
@@ -150,14 +251,67 @@ defmodule AshPhoenix.Form do
           changeset_opts
         )
     }
+    |> set_validity()
   end
 
+  @validate_opts [
+    errors: [
+      type: :boolean,
+      default: true,
+      doc: "Set to false to hide errors after validation"
+    ]
+  ]
+
+  @doc """
+  Validates the parameters against the form.
+
+  Options:
+
+  #{Ash.OptionsHelpers.docs(@validate_opts)}
+  """
+  @spec validate(t(), map, Keyword.t()) :: t()
+  def validate(form, new_params, opts \\ []) do
+    opts = Ash.OptionsHelpers.validate!(opts, @validate_opts)
+
+    new_form_opts =
+      form.opts
+      |> Keyword.put(:errors, opts[:errors])
+      |> Keyword.put(:params, new_params)
+      |> Keyword.put(:forms, form.form_keys)
+
+    case form.type do
+      :create ->
+        for_create(
+          form.resource,
+          form.action,
+          new_form_opts
+        )
+
+      :update ->
+        for_update(form.data, form.action, new_form_opts)
+
+      :destroy ->
+        for_destroy(form.data, form.action, new_form_opts)
+    end
+  end
+
+  @doc """
+  Submits the form by calling the appropriate function on the provided api.
+
+  For example, a form created with `for_update/3` will call `api.update(changeset)`, where
+  changeset is the result of passing the `Form.params/3` into `Ash.Changeset.for_update/4`.
+
+  If the submission returns an error, the resulting form can simply be rerendered. Any nested
+  errors will be passed down to the corresponding form for that input.
+  """
+  @spec submit(t(), Ash.Api.t(), Keyword.t()) ::
+          {:ok, Ash.Resource.record()} | :ok | {:error, t()}
   def submit(form, api, opts \\ []) do
     changeset_opts = Keyword.drop(form.opts, [:forms, :errors, :id, :method, :for, :as])
 
     form = clear_errors(form)
 
-    if form.source.valid? || opts[:force?] do
+    if form.valid? || opts[:force?] do
       result =
         case form.type do
           :create ->
@@ -177,9 +331,6 @@ defmodule AshPhoenix.Form do
         end
 
       case result do
-        {:ok, result} ->
-          {:ok, result}
-
         {:error, %{changeset: changeset} = error} ->
           changeset = %{changeset | errors: []}
 
@@ -189,49 +340,18 @@ defmodule AshPhoenix.Form do
             |> Enum.flat_map(&expand_error/1)
 
           {:error, set_action_errors(%{form | source: changeset}, errors)}
+
+        other ->
+          other
       end
     else
       {:error, form}
     end
   end
 
-  defp set_action_errors(form, errors, path \\ []) do
-    new_forms =
-      form.forms
-      |> Map.new(fn {key, forms} ->
-        config = form.form_keys[key]
-
-        new_forms =
-          if is_list(forms) do
-            forms
-            |> Enum.with_index()
-            |> Enum.map(fn {form, index} ->
-              set_action_errors(form, errors, path ++ [config[:for] || key, index])
-            end)
-          else
-            if forms do
-              set_action_errors(forms, errors, path ++ [config[:for] || key])
-            else
-              {key, forms}
-            end
-          end
-
-        {key, new_forms}
-      end)
-
-    %{form | submit_errors: transform_errors(form, errors, path), forms: new_forms}
-  end
-
-  defp expand_error(%class_mod{} = error)
-       when class_mod in [
-              Ash.Error.Forbidden,
-              Ash.Error.Framework,
-              Ash.Error.Invalid,
-              Ash.Error.Unkonwn
-            ] do
-    error.errors
-  end
-
+  @doc """
+  Same as `submit/3`, but raises an error if the submission fails.
+  """
   def submit!(form, api, opts \\ []) do
     changeset_opts = Keyword.drop(form.opts, [:forms, :errors, :id, :method, :for, :as])
 
@@ -259,8 +379,13 @@ defmodule AshPhoenix.Form do
     end
   end
 
-  def params(nil), do: nil
+  @doc """
+  Returns the parameters from the form that would be submitted to the action.
 
+  This can be useful if you want to get the parameters and manipulate them/build a custom changeset
+  afterwards.
+  """
+  @spec params(t()) :: map
   def params(form) do
     form_keys =
       form.form_keys
@@ -270,7 +395,14 @@ defmodule AshPhoenix.Form do
     Enum.reduce(form.form_keys, Map.drop(form.params, form_keys), fn {key, config}, params ->
       case config[:type] || :single do
         :single ->
-          Map.put(params, to_string(config[:for] || key), params(form.forms[key]))
+          nested_params =
+            if form.forms[key] do
+              params(form.forms[key])
+            else
+              nil
+            end
+
+          Map.put(params, to_string(config[:for] || key), nested_params)
 
         :list ->
           for_name = to_string(config[:for] || key)
@@ -284,6 +416,56 @@ defmodule AshPhoenix.Form do
     end)
   end
 
+  @add_form_opts [
+    prepend: [
+      type: :boolean,
+      default: false,
+      doc:
+        "If specified, the form is placed at the beginning of the list instead of the end of the list"
+    ],
+    params: [
+      type: :any,
+      default: %{},
+      doc: "The initial parameters to add the form with."
+    ]
+  ]
+
+  @doc """
+  Adds a new form at the provided path.
+
+  Doing this requires that the form has a `create_action` and a `resource` configured.
+
+  `path` can be one of two things:
+
+  1. A list of atoms and integers that lead to a form in the `forms` option provided. `[:posts, 0, :comments]` to add a comment to the first post.
+  2. The html name of the form, e.g `form[posts][0][comments]` to mimic the above
+
+  #{Ash.OptionsHelpers.docs(@add_form_opts)}
+  """
+  @spec add_form(t(), String.t() | list(atom | integer), Keyword.t()) :: t()
+  def add_form(form, path, opts \\ []) do
+    opts = Ash.OptionsHelpers.validate!(opts, @add_form_opts)
+
+    if is_binary(path) do
+      do_add_form(form, parse_path!(form, path), opts, [])
+    else
+      do_add_form(form, List.wrap(path), opts, [])
+    end
+  end
+
+  @doc """
+  Removes a form at the provided path.
+
+  See `add_form/3` for more information on the `path` argument.
+
+  If you are not using liveview, and you want to support removing forms that were created based on the `data`
+  option from the browser, you'll need to include in the form submission a custom list of strings to remove, and
+  then manually iterate over them in your controller, for example:
+
+  ```elixir
+  Enum.reduce(removed_form_paths, form, &AshPhoenix.Form.remove_form(&2, &1))
+  ```
+  """
   def remove_form(form, path) do
     if is_binary(path) do
       do_remove_form(form, parse_path!(form, path), [])
@@ -362,14 +544,6 @@ defmodule AshPhoenix.Form do
     raise ArgumentError, message: "Invalid Path: #{inspect(Enum.reverse(trail, path))}"
   end
 
-  def add_form(form, path, opts \\ []) do
-    if is_binary(path) do
-      do_add_form(form, parse_path!(form, path), opts, [])
-    else
-      do_add_form(form, List.wrap(path), opts, [])
-    end
-  end
-
   defp do_add_form(form, [key, i | rest], opts, trail) when is_integer(i) do
     unless form.form_keys[key] do
       raise AshPhoenix.Form.NoFormConfigured, field: key
@@ -396,6 +570,22 @@ defmodule AshPhoenix.Form do
         :list ->
           []
       end
+
+    new_config =
+      form.form_keys
+      |> Keyword.update!(key, fn config ->
+        if config[:data] do
+          Keyword.update!(config, :data, fn data ->
+            if is_function(data) do
+              fn original_data -> [nil | data.(original_data)] end
+            else
+              [nil | data]
+            end
+          end)
+        else
+          config
+        end
+      end)
 
     new_forms =
       form.forms
@@ -434,11 +624,64 @@ defmodule AshPhoenix.Form do
         end
       end)
 
-    %{form | forms: new_forms}
+    %{form | forms: new_forms, form_keys: new_config}
   end
 
   defp do_add_form(_form, path, _opts, trail) do
     raise ArgumentError, message: "Invalid Path: #{inspect(Enum.reverse(trail, path))}"
+  end
+
+  @spec set_validity(t()) :: t()
+  defp set_validity(form) do
+    %{form | valid?: valid?(form)}
+  end
+
+  defp valid?(form) do
+    if form.source.valid? do
+      Enum.empty?(form.forms) ||
+        Enum.all?(form.forms, fn {_, v} ->
+          v
+          |> List.wrap()
+          |> Enum.all?(&valid?/1)
+        end)
+    end
+  end
+
+  defp set_action_errors(form, errors, path \\ []) do
+    new_forms =
+      form.forms
+      |> Map.new(fn {key, forms} ->
+        config = form.form_keys[key]
+
+        new_forms =
+          if is_list(forms) do
+            forms
+            |> Enum.with_index()
+            |> Enum.map(fn {form, index} ->
+              set_action_errors(form, errors, path ++ [config[:for] || key, index])
+            end)
+          else
+            if forms do
+              set_action_errors(forms, errors, path ++ [config[:for] || key])
+            else
+              {key, forms}
+            end
+          end
+
+        {key, new_forms}
+      end)
+
+    %{form | submit_errors: transform_errors(form, errors, path), forms: new_forms}
+  end
+
+  defp expand_error(%class_mod{} = error)
+       when class_mod in [
+              Ash.Error.Forbidden,
+              Ash.Error.Framework,
+              Ash.Error.Invalid,
+              Ash.Error.Unkonwn
+            ] do
+    error.errors
   end
 
   defp clear_errors(nil), do: nil
@@ -557,6 +800,27 @@ defmodule AshPhoenix.Form do
                 |> indexed_list()
                 |> Enum.reduce({[], List.wrap(data)}, fn form_params, {forms, data} ->
                   case data do
+                    [nil | rest] ->
+                      create_action =
+                        opts[:create_action] ||
+                          raise AshPhoenix.Form.NoActionConfigured,
+                            path: Enum.reverse(trail, Enum.reverse(trail, [key])),
+                            action: :create_action
+
+                      resource =
+                        opts[:resource] ||
+                          raise AshPhoenix.Form.NoResourceConfigured,
+                            path: Enum.reverse(trail, [key])
+
+                      form =
+                        for_create(resource, create_action,
+                          params: form_params,
+                          forms: opts[:forms] || [],
+                          errors: error?
+                        )
+
+                      {[form | forms], rest}
+
                     [data | rest] ->
                       update_action =
                         opts[:update_action] ||
