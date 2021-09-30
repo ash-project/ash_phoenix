eacf05487b5fe27d897e72aac485139778b8a552
kernel-io <kernel-io@users.noreply.github.com>
2021-10-01 08:24:40 +1300

    improvement: improve typespec on errors/2 (#27)
    Co-authored-by: kernel-io <git@omnium.co>

--- lib/ash_phoenix/form/form.ex
+++ lib/ash_phoenix/form/form.ex
@@ -1102,7 +1102,12 @@ defmodule AshPhoenix.Form do
   #{Ash.OptionsHelpers.docs(@errors_opts)}
   """
   @spec errors(t(), Keyword.t()) ::
-          [{atom, {String.t(), Keyword.t()}}] | [String.t()] | [{atom, String.t()}]
+          ([{atom, {String.t(), Keyword.t()}}]
+           | [String.t()]
+           | [{atom, String.t()}])
+          | %{
+              list => [{atom, {String.t(), Keyword.t()}}] | [String.t()] | [{atom, String.t()}]
+            }
   def errors(form, opts \\ []) do
     opts = validate_opts_with_extra_keys(opts, @errors_opts)
 
