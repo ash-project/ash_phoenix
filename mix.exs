ad8a3f678b508d17766dcb57ba80ac024ff6fc74
Zach Daniel <zach@zachdaniel.dev>
2022-08-09 12:15:59 -0400

    improvement: update to latest ash

--- mix.exs
+++ mix.exs
@@ -72,7 +72,7 @@ defmodule AshPhoenix.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ash, ash_version("~> 1.52.0-rc.2")},
+      {:ash, ash_version("~> 1.53")},
       {:phoenix, "~> 1.5.6 or ~> 1.6.0"},
       {:phoenix_html, "~> 2.14 or ~> 3.0"},
       {:phoenix_live_view, "~> 0.15"},
