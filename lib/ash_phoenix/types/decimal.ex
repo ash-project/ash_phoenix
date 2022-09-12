02f4b546908c17a69c9a2d3d89ed6db415569ac8
ymijorski <ymijorski@gmail.com>
2022-09-12 19:20:19 +0300

    improvement: decimal protocols (#51)

---
+++ lib/ash_phoenix/types/decimal.ex
@@ -0,0 +1,7 @@
+defimpl Phoenix.HTML.Safe, for: Decimal do
+  defdelegate to_iodata(data), to: Decimal, as: :to_string
+end
+
+defimpl Phoenix.Param, for: Decimal do
+  defdelegate to_param(data), to: Decimal, as: :to_string
+end
