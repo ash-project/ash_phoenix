89f1a20afb74bb30d530be5e4c5e9053fd32d6de
Zach Daniel <zach@zachdaniel.dev>
2023-10-09 16:05:17 -0400

    docs: add small example

--- lib/mix/tasks/ash_phoenix.gen.live.ex
+++ lib/mix/tasks/ash_phoenix.gen.live.ex
@@ -1,6 +1,14 @@
 defmodule Mix.Tasks.AshPhoenix.Gen.Live do
   @moduledoc """
-  Generates liveviews for a resource
+  Generates liveviews for a given api and resource.
+
+  The api and resource must already exist, this task does not define them.
+
+  For example:
+
+  ```bash
+  mix ash_phoenix.gen.live ExistingApiName ExistingResourceName
+  ```
   """
   use Mix.Task
 
