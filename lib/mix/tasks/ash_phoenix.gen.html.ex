b023505801def92bf75c028bd8a085435ec4f4c3
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2023-10-13 18:13:46 +0200

    Refactoring

--- lib/mix/tasks/ash_phoenix.gen.html.ex
+++ lib/mix/tasks/ash_phoenix.gen.html.ex
@@ -31,18 +31,30 @@ defmodule Mix.Tasks.AshPhoenix.Gen.Html do
 
     keys = [:api, :resource, :singular, :plural]
     {opts, _, _} = OptionParser.parse(args, switches: Enum.map(keys, &{&1, :string}))
-    binding = Enum.map(keys, fn key -> {key, opts[key]} end)
 
-    source_path = Application.app_dir(:ash_phoenix, "priv/templates/ash_phoenix.gen.html")
+    unless Code.ensure_loaded?(resource_module(opts)) do
+      Mix.shell().info("The resource #{app_name()}.#{opts[:api]}.#{opts[:resource]} does not exist.")
+    else
+      source_path = Application.app_dir(:ash_phoenix, "priv/templates/ash_phoenix.gen.html")
+      resource_html_dir = Macro.underscore(opts[:resource]) <> "_html"
+
+      binding = Enum.map(keys, fn key -> {key, opts[key]} end)
+      binding = [{:route_prefix, Macro.underscore(opts[:plural])} | binding]
+      binding = [{:app_name, app_name()} | binding]
+      binding = [{:attributes, attributes(opts)} | binding]
+      assigns = Enum.into(binding, %{})
+
+      template_files(resource_html_dir, opts)
+      |> generate_files(assigns, source_path)
+
+      print_shell_instructions(opts[:resource], opts[:plural])
+    end
+  end
+
+  defp template_files(resource_html_dir, opts) do
     app_web_path = "lib/#{Macro.underscore(app_name())}_web"
-    resource_html_dir = Macro.underscore(opts[:resource]) <> "_html"
 
-    binding = [{:route_prefix, Macro.underscore(opts[:plural])} | binding]
-    binding = [{:app_name, app_name()} | binding]
-    binding = [{:attributes, attributes(opts)} | binding]
-    assigns = Enum.into(binding, %{})
-
-    template_files = %{
+    %{
       "index.html.heex" => "#{app_web_path}/controllers/#{resource_html_dir}/index.html.heex",
       "show.html.heex" => "#{app_web_path}/controllers/#{resource_html_dir}/show.html.heex",
       "resource_form.html.heex" =>
@@ -53,9 +65,6 @@ defmodule Mix.Tasks.AshPhoenix.Gen.Html do
         "#{app_web_path}/controllers/#{Macro.underscore(opts[:resource])}_controller.ex",
       "html.ex" => "#{app_web_path}/controllers/#{Macro.underscore(opts[:resource])}_html.ex"
     }
-
-    generate_files(template_files, assigns, source_path)
-    print_shell_instructions(opts[:resource], opts[:plural])
   end
 
   defp generate_files(template_files, assigns, source_path) do
@@ -81,8 +90,12 @@ defmodule Mix.Tasks.AshPhoenix.Gen.Html do
     """)
   end
 
-  defp attributes(opts) do
+  defp resource_module(opts) do
     Module.concat(["#{app_name()}.#{opts[:api]}.#{opts[:resource]}"])
+  end
+
+  defp attributes(opts) do
+    resource_module(opts)
     |> Ash.Resource.Info.attributes()
     |> Enum.map(&attribute_map/1)
     |> Enum.reject(&reject_attribute?/1)
