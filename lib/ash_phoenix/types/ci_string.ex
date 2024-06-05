ab0e6f0ac91770753d7a1c71d8e4db27a3692ae4
Zach Daniel <zach@zachdaniel.dev>
2024-06-05 15:17:40 -0400

    fix: properly (i.e safely) encode ci strings to iodata and params

--- lib/ash_phoenix/types/ci_string.ex
+++ lib/ash_phoenix/types/ci_string.ex
@@ -1,11 +1,11 @@
 defimpl Phoenix.HTML.Safe, for: Ash.CiString do
   def to_iodata(%Ash.CiString{} = ci_string) do
-    Ash.CiString.value(ci_string)
+    Phoenix.HTML.Safe.to_iodata(Ash.CiString.value(ci_string))
   end
 end
 
 defimpl Phoenix.Param, for: Ash.CiString do
   def to_param(ci_string) do
-    Ash.CiString.value(ci_string)
+    Phoenix.Param.to_param(Ash.CiString.value(ci_string))
   end
 end
