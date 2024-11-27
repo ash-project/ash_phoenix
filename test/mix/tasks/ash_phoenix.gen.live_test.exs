b80a24e672c1a2b0744df21c83d587309e06ffa0
Srikanth Kyatham <srikanth.1.kyatham@gmail.com>
2024-11-27 20:09:11 +0200

    Fixing format in ash phoenix gen live test

--- test/mix/tasks/ash_phoenix.gen.live_test.exs
+++ test/mix/tasks/ash_phoenix.gen.live_test.exs
@@ -217,85 +217,87 @@ defmodule Mix.Tasks.AshPhoenix.Gen.LiveTest do
       |> format_contents(index_path)
 
     show_path = "lib/ash_phoenix_web/live/artist_live/show.ex"
-    show_contents = """
-     defmodule AshPhoenixWeb.ArtistLive.Show do
-       use AshPhoenixWeb, :live_view
 
-       @impl true
-       def render(assigns) do
-         ~H\"\"\"
-         <.header>
-           Artist <%= @artist.id %>
-           <:subtitle>This is a artist record from your database.</:subtitle>
+    show_contents =
+      """
+       defmodule AshPhoenixWeb.ArtistLive.Show do
+         use AshPhoenixWeb, :live_view
 
-           <:actions>
-             <.link patch={~p"/Artists/\#{@artist}/show/edit"} phx-click={JS.push_focus()}>
-               <.button>Edit artist</.button>
-             </.link>
-           </:actions>
-         </.header>
+         @impl true
+         def render(assigns) do
+           ~H\"\"\"
+           <.header>
+             Artist <%= @artist.id %>
+             <:subtitle>This is a artist record from your database.</:subtitle>
 
-         <.list>
-           <:item title="Id"><%= @artist.id %></:item>
+             <:actions>
+               <.link patch={~p"/Artists/\#{@artist}/show/edit"} phx-click={JS.push_focus()}>
+                 <.button>Edit artist</.button>
+               </.link>
+             </:actions>
+           </.header>
 
-           <:item title="Name"><%= @artist.name %></:item>
-         </.list>
+           <.list>
+             <:item title="Id"><%= @artist.id %></:item>
 
-         <.back navigate={~p"/Artists"}>Back to Artists</.back>
+             <:item title="Name"><%= @artist.name %></:item>
+           </.list>
 
-         <.modal
-           :if={@live_action == :edit}
-           id="artist-modal"
-           show
-           on_cancel={JS.patch(~p"/Artists/\#{@artist}")}
-         >
-           <.live_component
-             module={AshPhoenixWeb.ArtistLive.FormComponent}
-             id={@artist.id}
-             title={@page_title}
-             action={@live_action}
-             current_user={@current_user}
-             artist={@artist}
-             patch={~p"/Artists/\#{@artist}"}
-           />
-         </.modal>
-         \"\"\"
+           <.back navigate={~p"/Artists"}>Back to Artists</.back>
+
+           <.modal
+             :if={@live_action == :edit}
+             id="artist-modal"
+             show
+             on_cancel={JS.patch(~p"/Artists/\#{@artist}")}
+           >
+             <.live_component
+               module={AshPhoenixWeb.ArtistLive.FormComponent}
+               id={@artist.id}
+               title={@page_title}
+               action={@live_action}
+               current_user={@current_user}
+               artist={@artist}
+               patch={~p"/Artists/\#{@artist}"}
+             />
+           </.modal>
+           \"\"\"
+         end
+
+         @impl true
+         def mount(_params, _session, socket) do
+           {:ok, socket}
+         end
+
+         @impl true
+         def handle_params(%{"id" => id}, _, socket) do
+           {:noreply,
+            socket
+            |> assign(:page_title, page_title(socket.assigns.live_action))
+            |> assign(:artist, Ash.get!(AshPhoenix.Test.Artist, id, actor: socket.assigns.current_user))}
+         end
+
+         defp page_title(:show), do: "Show Artist"
+         defp page_title(:edit), do: "Edit Artist"
        end
-
-       @impl true
-       def mount(_params, _session, socket) do
-         {:ok, socket}
-       end
-
-       @impl true
-       def handle_params(%{"id" => id}, _, socket) do
-         {:noreply,
-          socket
-          |> assign(:page_title, page_title(socket.assigns.live_action))
-          |> assign(:artist, Ash.get!(AshPhoenix.Test.Artist, id, actor: socket.assigns.current_user))}
-       end
-
-       defp page_title(:show), do: "Show Artist"
-       defp page_title(:edit), do: "Edit Artist"
-     end
-    """
-    |> format_contents(show_path)
+      """
+      |> format_contents(show_path)
 
     assert Igniter.new()
-    |> Igniter.compose_task("ash_phoenix.gen.live", [
-      "--domain",
-      "Elixir.AshPhoenix.Test.Domain",
-      "--resource",
-      "Elixir.AshPhoenix.Test.Artist",
-      "--resourceplural",
-      "Artists"
-    ])
-    |> assert_creates(
-      form_path,
-      form_contents
-    )
-    |> assert_creates(index_path, index_contents)
-    |> assert_creates(show_path, show_contents)
+           |> Igniter.compose_task("ash_phoenix.gen.live", [
+             "--domain",
+             "Elixir.AshPhoenix.Test.Domain",
+             "--resource",
+             "Elixir.AshPhoenix.Test.Artist",
+             "--resourceplural",
+             "Artists"
+           ])
+           |> assert_creates(
+             form_path,
+             form_contents
+           )
+           |> assert_creates(index_path, index_contents)
+           |> assert_creates(show_path, show_contents)
   end
 
   defp format_contents(contents, path) do
