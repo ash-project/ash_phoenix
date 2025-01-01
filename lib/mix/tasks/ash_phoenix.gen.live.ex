0a1afd9bdb6becd2b4b4b46fd9aa620f27b45aeb
Victor <victor.kryukov@gmail.com>
2025-01-01 14:36:52 +0100

    Update ash_phoenix.gen.live.ex
    Use correct option switch

--- lib/mix/tasks/ash_phoenix.gen.live.ex
+++ lib/mix/tasks/ash_phoenix.gen.live.ex
@@ -2,7 +2,7 @@ if Code.ensure_loaded?(Igniter) do
   defmodule Mix.Tasks.AshPhoenix.Gen.Live do
     use Igniter.Mix.Task
 
-    @example "mix ash_phoenix.gen.live --domain ExistingDomainName --resource ExistingResourceName --resource-plural ExistingResourceNames"
+    @example "mix ash_phoenix.gen.live --domain ExistingDomainName --resource ExistingResourceName --resourceplural ExistingResourceNames"
 
     @shortdoc "Generates liveviews for a given domain and resource."
 
