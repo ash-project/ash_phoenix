9f79a8cecb2a6e06f7994fc45be3f2c282f984d7
Kenneth Kostrešević <kenneth.kostresevic@gmail.com>
2025-03-24 16:32:28 +0100

    fix: Prevent empty errors pass to error class (#332)

--- lib/ash_phoenix/form/form.ex
+++ lib/ash_phoenix/form/form.ex
@@ -2217,12 +2217,18 @@ defmodule AshPhoenix.Form do
   end
 
   defp carry_over_errors(form, additional_errors \\ nil) do
+    errors = List.wrap(form.source.errors)
+
     {these_errors, further_path_errors} =
-      form.source.errors
-      |> Ash.Error.to_error_class()
-      |> Map.get(:errors)
-      |> Enum.concat(additional_errors || [])
-      |> Enum.split_with(&(&1.path == []))
+      if errors == [] do
+        Enum.split_with(additional_errors || [], &(&1.path == []))
+      else
+        errors
+        |> Ash.Error.to_error_class()
+        |> Map.get(:errors)
+        |> Enum.concat(additional_errors || [])
+        |> Enum.split_with(&(&1.path == []))
+      end
 
     # not the first iteration
     form =
