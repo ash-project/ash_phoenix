f89f2f8db4e19c533f233a1b07cebc8f29d2ddc8
Matt Savoia <matt@1-800-rad-dude.com>
2025-06-19 11:52:03 -0400

    Update html task, wrap in code sippet

--- lib/mix/tasks/ash_phoenix.gen.html.ex
+++ lib/mix/tasks/ash_phoenix.gen.html.ex
@@ -8,7 +8,9 @@ defmodule Mix.Tasks.AshPhoenix.Gen.Html do
 
   #{AshPhoenix.Gen.docs()}
 
+  ```bash
   mix ash_phoenix.gen.html MyApp.Shop MyApp.Shop.Product --plural-name products
+  ```
   """
 
   def run([]) do
